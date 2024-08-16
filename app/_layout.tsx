1801fd09c3525c7fc851a87450a56c0658a961f8
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2024-08-16 20:41:55 +0200

    gluestack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,6 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import '@/global.css';
+import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
@@ -29,13 +31,16 @@ export default function RootLayout() {
   }
 
   return (
-    <ApplicationProvider {...eva} theme={colorScheme === 'dark' ? eva.dark : eva.light}>
-      {/* <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}> */}
+    // <GluestackUIProvider mode={colorScheme === 'dark' ? 'dark' : 'system'}>
+    <GluestackUIProvider mode='light'>
+      {/* <ApplicationProvider {...eva} theme={colorScheme === 'dark' ? eva.dark : eva.light}> */}
+        {/* <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}> */}
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
-      {/* </ThemeProvider> */}
-    </ApplicationProvider>
+        {/* </ThemeProvider> */}
+      {/* </ApplicationProvider */}
+    </GluestackUIProvider>
   );
 }
