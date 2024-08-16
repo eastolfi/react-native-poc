f7294d7f4860cd51c8ffc1902927c172e0191201
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2024-08-16 18:36:32 +0200

    ui-kitten

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -27,11 +29,13 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <ApplicationProvider {...eva} theme={colorScheme === 'dark' ? eva.dark : eva.light}>
+      {/* <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}> */}
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      {/* </ThemeProvider> */}
+    </ApplicationProvider>
   );
 }
