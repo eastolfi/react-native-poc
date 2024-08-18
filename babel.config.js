555aedeff6b84da77e215ad56690778d22ca1546
Eduardo Astolfi <astolfi_wotc@hotmail.com>
2024-08-18 16:19:47 +0200

    rnui

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,13 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: [
+          '@babel/plugin-transform-export-namespace-from',
+          'react-native-reanimated/plugin',
+        ],
+      },
+    },
   };
 };
